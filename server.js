770da59f772ca696b1b91b3edb955bdd7bef2756
MetAIvisuals <115116436+MetAIvisuals@users.noreply.github.com>
2026-02-27 15:38:39 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
+
 // Create HTTP server
 const server = http.createServer(app);
 
